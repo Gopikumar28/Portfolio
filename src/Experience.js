11f11f6a9b6e683641bdae00a73ed716b223bf3b
Gopikumar28 <gopikumar281998@gmail.com>
2024-05-15 23:50:03 +0530

    Deployed website1

--- src/Experience.js
+++ src/Experience.js
@@ -30,7 +30,7 @@ export const Experience = () => {
                               <Col sm=  {1}> </Col> 
 
                               <Col sm = {4}>
-                              <img src = {TATA} alt='TCS LOGO' style={{height:"100px"}} />
+                              <img src = {TATA} alt='TCS LOGO' style={{height:"90%", width:"90%"}} />
                               </Col>
 
                               <Col sm=  {1}> </Col> 
@@ -54,7 +54,7 @@ export const Experience = () => {
 
                               <Col sm = {4}>
                                 
-                              <img src = {THAAGAM} alt='Thaagam Foundation LOGO' style={{height:"70px"}} />
+                              <img src = {THAAGAM} alt='Thaagam Foundation LOGO' style={{height:"90%", width:"90%"}} />
                               </Col>
 
                               <Col sm=  {1}> </Col> 
